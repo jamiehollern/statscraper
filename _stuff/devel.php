f405c3086525ae7de4a31716e109850e30b02a1b
Jamie Hollern <jamie.hollern@digirati.co.uk>
2016-06-11 23:18:38 +0100

    All the files.

---
+++ _stuff/devel.php
@@ -0,0 +1,8 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+ini_set('xdebug.var_display_max_depth', -1);
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_data', -1);
---
+++ scrapeurls.php
@@ -0,0 +1,44 @@
+<?php
+
+$url = isset($_GET['url']) ? $_GET['url'] : '';
+if ($url) {
+  // Get the webpage and load it into a dom document.
+  $html = file_get_contents($url);
+  $doc = new DOMDocument();
+  // This class doesn't like HTML5 so supress the error.
+  @$doc->loadHTML($html);
+  // Convert it to simplexml.
+  $sxml = simplexml_import_dom($doc);
+  // Get all of the divs with fixtures in them.
+  $fixture_divs = $sxml->body->xpath('//div[@class="fixtures"]');
+  $links = [];
+  // Iterate through them.
+  foreach ($fixture_divs as $div) {
+    $teams = $div->xpath('//div[@class="teams"]');
+    foreach ($teams as $team) {
+      $fixture = (string) $team->a->attributes()->href;
+      $links[] = 'http://spfl.co.uk' . $fixture;
+    }
+  }
+  $links = array_unique($links);
+  if ($links) {
+    $json = json_encode($links);
+    $fixtures_path = './data/fixtures';
+    if (!file_exists($fixtures_path)) {
+      mkdir($fixtures_path);
+    }
+    $filepath = realpath('./data/fixtures');
+    $url_parts = parse_url($url);
+    $filename = $filepath . '/' . preg_replace('/[^ \w]+/', '', $url_parts['path']) . '.json';
+    $write = file_put_contents($filename, $json);
+    if ($write) {
+      print 'JSON file ' . $filename . ' written.';
+    }
+    else {
+      print 'JSON file ' . $filename . ' failed to write.';
+    }
+  }
+  else {
+    print "Couldn't figure this one out.";
+  }
+}
